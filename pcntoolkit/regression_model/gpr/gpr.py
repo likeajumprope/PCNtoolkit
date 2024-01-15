9271a53f25236184c3fdca57beb88284252ec4fe
likeajumprope <hannabayer.89@gmail.com>
2024-01-15 12:23:32 +0100

    add predict logik in gpr.py

--- pcntoolkit/regression_model/gpr/gpr.py
+++ pcntoolkit/regression_model/gpr/gpr.py
@@ -19,18 +19,54 @@ class GPR:
         raise NotImplementedError(
             f"Fit method not implemented for {self.__class__.__name__}")
 
-    def predict(self, X: np.ndarray) -> np.ndarray:
+    def predict(self, X: np.ndarray, hyp, y: np.ndarray, Xs: np.ndarray) -> np.ndarray:
         """
         Predicts on new data.
         """
-        # some prediction logic
+        if len(hyp.shape) > 1:  # force 1d hyperparameter array
+            hyp = hyp.flatten()
+
+        # ensure X and Xs are multi-dimensional arrays
+        if len(Xs.shape) == 1:
+            Xs = Xs[:, np.newaxis]
+        if len(X.shape) == 1:
+            X = X[:, np.newaxis]
+
+        # parameters for warping the likelhood function
+        if self.warp is not None:
+            gamma = hyp[1:(self.n_warp_param+1)]
+            y = self.warp.f(y, gamma)
+
+        # reestimate posterior (avoids numerical problems with optimizer)
+        self.post(hyp, self.covfunc, X, y)
+
+        # hyperparameters
+        sn2 = np.exp(2*hyp[0])     # noise variance
+        # (generic) covariance hyperparameters
+        theta = hyp[(self.n_warp_param + 1):]
+
+        Ks = self.covfunc.cov(theta, Xs, X)
+        kss = self.covfunc.cov(theta, Xs)
+
+        # predictive mean
+        ymu = Ks.dot(self.alpha)
+
+        # predictive variance (for a noisy test input)
+        v = solve(self.L, Ks.T)
+        ys2 = kss - v.T.dot(v) + sn2
+
+        return ymu, ys2
+  
         # ...
         raise NotImplementedError(
             f"Predict method not implemented for {self.__class__.__name__}")
 
     def fit_predict(self, X: np.ndarray, y: np.ndarray, X_test) -> np.ndarray:
         """
-        Fits and predicts the model.
+        Fits and predicts the model. Used to be estimate.
+
+
+        
         """
         # some fit_predict logic
         # ...
