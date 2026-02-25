fix - prevent overflow and underflow of hyperparameters
Hyperparameters (alpha and beta) do not flow to very small values (close to 0) or to inf. This allows A (posterior precision matrix) to be inverted and the posterior distribution to be computed

pcntoolkit/regression_model/blr.py
@@ -588,18 +588,28 @@ class BLR(RegressionModel):
         tuple[np.ndarray, np.ndarray, np.ndarray]
             Parsed alpha, beta and gamma parameters.
         """
+        # Avoid overflow of np.exp by computing the largest number we can
+        # safely exponentiate in float64
+        MAX_EXP: float = np.log(np.finfo(np.float64).max)
+
         N = Phi.shape[0]
         beta: np.ndarray = None  # type: ignore
         # Noise precision
         if self.models_variance:
             if Phi_var is None or (Phi_var == 0).all():
-                raise ValueError(Output.error(Errors.ERROR_BLR_VAR_X_NOT_PROVIDED))
+                raise ValueError(
+                    Output.error(Errors.ERROR_BLR_VAR_X_NOT_PROVIDED)
+                )
             Dv = Phi_var.shape[1]
             w_d = np.asarray(hyp[0:Dv])
-            beta = np.exp(Phi_var.dot(w_d))
+            # Dont allow the exponent to overflow to inf
+            beta = np.exp(
+                np.clip(Phi_var.dot(w_d), -MAX_EXP, MAX_EXP)
+            )
             n_lik_param = len(w_d)
         else:
-            beta = np.asarray([np.exp(hyp[0])])
+            # Dont allow the exponent to overflow to inf
+            beta = np.asarray([np.exp(np.clip(hyp[0], -MAX_EXP, MAX_EXP))])
             n_lik_param = len(beta)
 
         if self.warp:
@@ -621,9 +631,13 @@ class BLR(RegressionModel):
 
         # Coefficients precision
         if isinstance(beta, list) or isinstance(beta, np.ndarray):
-            alpha = np.exp(hyp[n_lik_param:])
+            # Dont allow the exponent to overflow to inf
+            alpha = np.exp(
+                np.clip(hyp[n_lik_param:], -MAX_EXP, MAX_EXP)
+            )
         else:
-            alpha = np.exp(hyp[1:])
+            # Dont allow the exponent to overflow to inf
+            alpha = np.exp(np.clip(hyp[1:], -MAX_EXP, MAX_EXP))
 
         return alpha, beta, gamma  # type: ignore
 
@@ -672,9 +686,15 @@ class BLR(RegressionModel):
         else:
             raise ValueError(Output.error(Errors.BLR_HYPERPARAMETER_VECTOR_INVALID_LENGTH))
 
-        # Compute the posterior precision and mean
+        # Compute the posterior precision matrix A
         XtLambda_n = X.T * self.lambda_n_vec
         self.A = XtLambda_n.dot(X) + self.Lambda_a
+        # Add a small value to the diagonal of A to ensure that inv(A) can be
+        # computed when the optimizer estimates extreme hyperparameter values
+        # (i.e., close to 0 or infinity).
+        self.A = self.A + 0.001 * np.eye(self.A.shape[0])
+
+        # Compute the posterior mean m
         invAXt: np.ndarray = linalg.solve(self.A, X.T, check_finite=False)
         self.m = (invAXt * self.lambda_n_vec).dot(y)
 